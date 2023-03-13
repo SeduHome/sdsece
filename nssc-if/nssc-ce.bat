79ae15ec170c7c8e8f82f2fcbaa14113f3f75565
SeduHome <125974063+SeduHome@users.noreply.github.com>
2023-03-13 13:18:57 +0100

    Add files via upload

--- nssc-if/nssc-ce.bat
+++ nssc-if/nssc-ce.bat
@@ -2,33 +2,9 @@
 cls
 taskkill /f /pid explorer.exe
 cls
-timeout /t 4 /nobreak >nul
+timeout /t 2 /nobreak >nul
 start explorer.exe
 cls
 timeout /t 3 /nobreak >nul
-taskkill /f /IM ActApp.exe /t
-taskkill /f /IM client32.exe /t
-taskkill /f /IM DBI.exe /t
-taskkill /f /IM ismetro.exe /t
-taskkill /f /IM keyshow.exe /t
-taskkill /f /IM keyShow64.exe /t
-taskkill /f /IM nsmexec.exe /t
-taskkill /f /IM NSSilence.exe /t
-taskkill /f /IM NSToast.exe /t
-taskkill /f /IM pcicfgui.exe /t
-taskkill /f /IM pcijoin.exe /t
-taskkill /f /IM PCINSSCD.exe /t
-taskkill /f /IM PCIVIDEO.exe /t
-taskkill /f /IM pcivideovi.exe /t
-taskkill /f /IM PlaySound.exe /t
-taskkill /f /IM remcmdstub.exe /t
-taskkill /f /IM runplugin.exe /t
-taskkill /f /IM Runplugin64.exe /t
-taskkill /f /IM schplayer.exe /t
-taskkill /f /IM setroom.exe /t
-taskkill /f /IM StudentUI.exe /t
-taskkill /f /IM supporttool.exe /t
-taskkill /f /IM winst64.exe /t
-taskkill /f /IM WINSTALL.exe /t
 PowerShell.exe -ExecutionPolicy Unrestricted -command "%~dp0\nssc-esce.ps1"
 exit
