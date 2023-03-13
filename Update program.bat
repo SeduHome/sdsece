7c94c356e9875d4bc2148b30461e28827e5ce384
SeduHome <125974063+SeduHome@users.noreply.github.com>
2023-03-13 12:33:06 +0100

    Add files via upload

--- Update program.bat
+++ Update program.bat
@@ -2,22 +2,17 @@
 cls
 color 11
 cls
-type %~dp0\nssc-if\nssc-ut.txt
-timeout /t 4 /nobreak >nul
+type %~dp0\nssc-if\nssc-sut.txt
+timeout /t 3 /nobreak >nul
 cls
 del %~dp0\nssc-if\* /q
-cls
 del %~dp0\Version.txt /q
 cls
 timeout /t 1 /nobreak >nul
-cls
-curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-rs.bat > %~dp0\nssc-if\nssc-rs.bat
-cls
-curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-ce.bat > %~dp0\nssc-if\nssc-ce.bat
-cls
-curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-esce.ps1 > %~dp0\nssc-if\nssc-esce.ps1
-cls
-curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-ut.txt > %~dp0\nssc-if\nssc-ut.txt
-cls
+type %~dp0\nssc-if\nssc-ut.txt
+curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-if/nssc-rs.bat > %~dp0\nssc-if\nssc-rs.bat
+curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-if/nssc-ce.bat > %~dp0\nssc-if\nssc-ce.bat
+curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-if/nssc-esce.ps1 > %~dp0\nssc-if\nssc-esce.ps1
+curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-if/nssc-sut.txt > %~dp0\nssc-if\nssc-sut.txt
+curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-if/nssc-ut.txt > %~dp0\nssc-if\nssc-ut.txt
 curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/Version.txt > %~dp0\Version.txt
-cls
