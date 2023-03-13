3a7eb577ab66b58c8dffc4b02a3287d2e352e391
SeduHome <125974063+SeduHome@users.noreply.github.com>
2023-03-13 12:50:59 +0100

    Add files via upload

--- Update program.bat
+++ Update program.bat
@@ -2,13 +2,14 @@
 cls
 color 11
 type %~dp0\nssc-if\nssc-sut.txt
-timeout /t 3 /nobreak >nul
+timeout /t 2 /nobreak >nul
 cls
+type %~dp0\nssc-if\nssc-ut.txt
+timeout /t 2 /nobreak >nul
 del %~dp0\nssc-if\* /q
 del %~dp0\Version.txt /q
 cls
 timeout /t 1 /nobreak >nul
-type %~dp0\nssc-if\nssc-ut.txt
 curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-if/nssc-rs.bat > %~dp0\nssc-if\nssc-rs.bat
 curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-if/nssc-ce.bat > %~dp0\nssc-if\nssc-ce.bat
 curl https://raw.githubusercontent.com/SeduHome/nssc-cep/main/nssc-if/nssc-esce.ps1 > %~dp0\nssc-if\nssc-esce.ps1
